d6a69bbdc3af4c409bb924b90dca94a5c800f8cb
devthejo <devthejo@protonmail.com>
2025-08-09 11:01:30 +0200

    fix: menu index typo

--- src/navigation/DrawerNav/DrawerItemList.js
+++ src/navigation/DrawerNav/DrawerItemList.js
@@ -1,6 +1,9 @@
 import SectionSeparator from "./SectionSeparator";
 import menuItem from "./menuItem";
 
+const index1 = 5;
+const index2 = 9;
+
 export default function DrawerItemList(props) {
   const { state } = props;
   const { index } = state;
@@ -17,18 +20,18 @@ export default function DrawerItemList(props) {
 
   const { routes } = state;
 
-  const section1 = routes.slice(0, 5);
-  const section2 = routes.slice(5, 9);
-  const section3 = routes.slice(9, routes.length);
+  const section1 = routes.slice(0, index1);
+  const section2 = routes.slice(index1, index2);
+  const section3 = routes.slice(index2, routes.length);
 
   return (
     <>
       <SectionSeparator label="Mon compte" />
-      {section2.map((props, i) => routeMenuItem(props, i + 5))}
+      {section2.map((props, i) => routeMenuItem(props, i + index1))}
       <SectionSeparator label="Alerter" />
       {section1.map((props, i) => routeMenuItem(props, i + 0))}
       <SectionSeparator label="Infos pratiques" />
-      {section3.map((props, i) => routeMenuItem(props, i + 8))}
+      {section3.map((props, i) => routeMenuItem(props, i + index2))}
     </>
   );
 }
