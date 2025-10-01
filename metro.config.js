fd6ba1caab783b13bec45584a8db7f12adc1e9c3
devthejo <devthejo@protonmail.com>
2025-10-01 11:41:08 +0200

    fix(android): local dev + wip

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,7 @@
 const { getDefaultConfig: getExpoDefaultConfig } = require("expo/metro-config");
 const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const { getSentryExpoConfig } = require("@sentry/react-native/metro");
+const exclusionList = require("metro-config/src/defaults/exclusionList");
 
 const defaultConfig = getDefaultConfig(__dirname);
 const sentryConfig = getSentryExpoConfig(__dirname);
@@ -9,6 +10,10 @@ const sentryConfig = getSentryExpoConfig(__dirname);
 const config = {
   resolver: {
     ...sentryConfig.resolver,
+    // Ignore transient JNI output folders to prevent ENOENT watchers on missing ABI directories
+    blockList: exclusionList([
+      /node_modules\/.*\/android\/build\/intermediates\/(library_jni|merged_jni_libs)\/.*/,
+    ]),
     sourceExts: [...sentryConfig.resolver.sourceExts, "cjs"],
     assetExts: [...defaultConfig.resolver.assetExts, "ttf"],
   },
